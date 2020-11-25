0dbb4f66471062ad3598cb7e676f8614199481df
Cenk Kılıç <cenk@kilic.dev>
2020-11-25 12:00:32 +0100

    chore: retry

--- packages/brownie/release.config.js
+++ packages/brownie/release.config.js
@@ -9,13 +9,6 @@ module.exports = {
         assets: [ 'CHANGELOG.md', 'README.md', 'API.md' ]
       }
     ],
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd: 'NPM_USER=$PLUGIN_NPM_USERNAME NPM_PASS=$PLUGIN_NPM_PASSWORD NPM_EMAIL=$PLUGIN_NPM_EMAIL npm-cli-login && npm whoami && yarn docs:readme && yarn docs:toc',
-        failCmd: 'echo "Building failed." && exit 127'
-      }
-    ],
     '@semantic-release/npm'
   ]
 }
--- packages/nx-nest/release.config.js
+++ packages/nx-nest/release.config.js
@@ -9,15 +9,6 @@ module.exports = {
         assets: [ 'CHANGELOG.md', 'README.md', 'API.md' ]
       }
     ],
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd:
-          // eslint-disable-next-line max-len
-          'NPM_REGISTRY=https://verdaccio.dev.webundsoehne.com NPM_USER=$PLUGIN_NPM_USERNAME NPM_PASS=$PLUGIN_NPM_PASSWORD NPM_EMAIL=$PLUGIN_NPM_EMAIL npm-cli-login && npm whoami && yarn docs:toc && yarn docs:jsdoc',
-        failCmd: 'echo "Building failed." && exit 127'
-      }
-    ],
     '@semantic-release/npm'
   ]
 }
