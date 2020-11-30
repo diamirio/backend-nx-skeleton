af71a2eb6d4abf7fc6e35b9b7e1bfd23869f2824
Cenk Kılıç <cenk@kilic.dev>
2020-11-30 14:30:12 +0100

    fix: added eslint ignore to ts-ignores to orm configurition, refactored jest config

--- packages/nx-nest/src/schematics/application/files/tests/jest.config.js
+++ packages/nx-nest/src/schematics/application/files/tests/jest.config.js
@@ -1,35 +1,24 @@
-const path = require('path')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
 
-const TS_CONFIG_PATH = path.join(process.cwd(), 'tsconfig.json')
-const SRC_PATH = path.join(process.cwd(), 'src')
-
-function makeModuleNameMapper(srcPath, tsconfigPath) {
-  // Get paths from tsconfig
-  const { paths } = require(tsconfigPath).compilerOptions
-
-  const aliases = {}
-
-  // Iterate over paths and convert them into moduleNameMapper format
-  Object.keys(paths).forEach((item) => {
-    const key = item.replace('/*', '/(.*)')
-    const path = paths[item][0].replace('/*', '/$1')
-    aliases[key] = srcPath + '/' + path
-  })
-  return aliases
-}
+const TS_CONFIG_PATH = 'tests/tsconfig.json'
+const SRC_PATH = 'src'
 
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   rootDir: '../',
   testEnvironment: 'node',
-  testRegex: '(/test/.*|/src/.*).(e2e-)?spec.tsx?$',
+  testRegex: '(/tests/.*|/src/.*).(e2e-)?spec.tsx?$',
   transform: {
     '^.+\\.(t)s$': 'ts-jest'
   },
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tests/tsconfig.json'
+      tsconfig: join('<rootDir>', TS_CONFIG_PATH)
     }
   },
-  moduleNameMapper: makeModuleNameMapper(SRC_PATH, TS_CONFIG_PATH)
+  moduleNameMapper: pathsToModuleNameMapper(JSON.parse(readFileSync(join(process.cwd(), TS_CONFIG_PATH))).compilerOptions.paths, {
+    prefix: `<rootDir>/${SRC_PATH}`
+  })
 }
