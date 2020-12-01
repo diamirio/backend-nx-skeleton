8a92046459b95635c8e4032d8418073649186523
Cenk Kılıç <cenk@kilic.dev>
2020-12-02 00:37:45 +0100

    fix: negation deleted

--- packages/nestjs-util/src/provider/microservice-client/microservice-provider.service.ts
+++ packages/nestjs-util/src/provider/microservice-client/microservice-provider.service.ts
@@ -84,7 +84,7 @@ export class MicroserviceProviderService<
   ): Promise<ReturnValue> {
     const o = { ...this.options, ...options }
 
-    if (this.clients[queue]) {
+    if (!this.clients[queue]) {
       throw new Error(
         `"${queue}" is not available in the context of this provider. Please check MicroserviceProviderModule.forRoot inputs.`
       )
