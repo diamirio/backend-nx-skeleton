fa8aada052a8dcfca733005118d76bc0b751c259
Cenk Kılıç <cenk@kilic.dev>
2020-12-04 11:04:32 +0100

    chore: ignore linting rule in config interface

--- packages/brownie/src/interfaces/default-config.interface.ts
+++ packages/brownie/src/interfaces/default-config.interface.ts
@@ -3,6 +3,7 @@ import { BaseConfig } from '@cenk1cenk2/boilerplate-oclif'
 import { NodeDependency } from './dependency.interface'
 
 export interface Configuration extends BaseConfig {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   package_registry?: string
   workspace: {
     requiredDependencies: NodeDependency[]
