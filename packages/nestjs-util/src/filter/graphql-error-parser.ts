4c486682633f0837f5910571cbc412d0814f5b07
Cenk Kılıç <cenk@kilic.dev>
2020-12-13 17:41:37 +0100

    fix: updated validation pipe errors for graphql

--- packages/nestjs-util/src/filter/graphql-error-parser.ts
+++ packages/nestjs-util/src/filter/graphql-error-parser.ts
@@ -3,14 +3,23 @@ import { GraphQLError } from 'graphql'
 import { EOL } from 'os'
 
 import { EnrichedException } from './exception.interface'
-import { getErrorMessage, logErrorDebugMsg } from './util'
+import { formatValidationError, getErrorMessage, isValidationError, logErrorDebugMsg } from './util'
 
 export function GraphQLErrorParser (exception: GraphQLError): EnrichedException {
-  const payload = {
+  let payload = {
     statusCode: exception.extensions?.exception?.response?.statusCode ?? HttpStatus.INTERNAL_SERVER_ERROR,
     error: exception?.name ?? exception.message,
     message: getErrorMessage(exception),
     path: exception.path?.map((i) => i.toString())
+  }
+
+  if (isValidationError(exception.extensions.exception)) {
+    const errors = formatValidationError(exception.extensions.exception.validation)
+
+    payload = {
+      ...payload,
+      message: errors.map((err) => err.messages.join(' | ')).join(' | ')
+    }
   }
 
   logErrorDebugMsg(new Logger('GraphQLErrorParser'), payload, exception.extensions?.exception.stacktrace.join(EOL))
