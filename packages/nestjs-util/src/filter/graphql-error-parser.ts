3dc7a84c986bcc11eed68868dc8b8243f88a6c4b
Cenk Kılıç <cenk@kilic.dev>
2021-01-05 19:13:37 +0100

    fix: try to bandaid with rewire will try it out

--- packages/nestjs-util/src/filter/graphql-error-parser.ts
+++ packages/nestjs-util/src/filter/graphql-error-parser.ts
@@ -1,6 +1,7 @@
 import { HttpStatus, Logger } from '@nestjs/common'
 import { GraphQLError, GraphQLFormattedError } from 'graphql'
 import { EOL } from 'os'
+import rewire from 'rewire'
 
 import { EnrichedException } from './exception.interface'
 import { formatValidationError, getErrorMessage, isValidationError, logErrorDebugMsg } from './util'
@@ -33,3 +34,23 @@ export function GraphQLErrorParser (exception: GraphQLError): GraphQLFormattedEr
     extensions: payload
   }
 }
+
+// ugly bandaid for this change.
+// https://github.com/graphql/graphql-js/pull/2731
+// eslint-disable-next-line no-underscore-dangle
+rewire('graphql/error/locatedError').__set__('locatedError', (originalError: any, nodes: any, path?: any) => {
+  // Sometimes a non-error is thrown, wrap it as an Error instance to ensure a consistent Error interface.
+  // Note: this uses a brand-check to support GraphQL errors originating from other contexts.
+  if (Array.isArray(originalError.path)) {
+    return originalError
+  }
+
+  return new GraphQLError(
+    originalError.message,
+    originalError.nodes ?? nodes,
+    originalError.source,
+    originalError.positions,
+    path,
+    originalError
+  )
+})
