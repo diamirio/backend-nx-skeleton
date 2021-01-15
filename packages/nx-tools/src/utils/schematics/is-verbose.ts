03c017056f382368023d5e423542cfbea74bdcb5
Cenk Kılıç <cenk@kilic.dev>
2021-01-15 16:58:07 +0100

    fix: adds environment variable on import of isverbose

--- packages/nx-tools/src/utils/schematics/is-verbose.ts
+++ packages/nx-tools/src/utils/schematics/is-verbose.ts
@@ -4,3 +4,8 @@
 export function isVerbose (): boolean {
   return process.argv.indexOf('--verbose') >= 0
 }
+
+// Enable debugging logger if in verbose mode
+if (isVerbose()) {
+  process.env.NG_DEBUG = '1'
+}
