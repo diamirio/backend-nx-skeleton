0beaf24e2a04f45e4270127748df8247d3cc39fd
Cenk Kılıç <cenk@kilic.dev>
2021-01-15 17:11:49 +0100

    fix: update debug mode to use the env variable as in angular

--- packages/nx-tools/src/utils/schematics/is-verbose.ts
+++ packages/nx-tools/src/utils/schematics/is-verbose.ts
@@ -2,10 +2,5 @@
  * Returns if angular cli is running with verbose flag.s
  */
 export function isVerbose (): boolean {
-  return process.argv.indexOf('--verbose') >= 0
-}
-
-// Enable debugging logger if in verbose mode
-if (isVerbose()) {
-  process.env.NG_DEBUG = '1'
+  return process.argv.indexOf('--verbose') >= 0 || process.env?.NG_DEBUG === '1'
 }
