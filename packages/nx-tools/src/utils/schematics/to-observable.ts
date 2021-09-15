88ab5a9a7a2751505b12d673343db2288c2fea0a
Cenk <cenk@kilic.dev>
2021-09-15 21:11:05 +0200

    fix: rxjs breaking change

--- packages/nx-tools/src/utils/schematics/to-observable.ts
+++ packages/nx-tools/src/utils/schematics/to-observable.ts
@@ -1,18 +1,26 @@
 import { BuilderOutput } from '@angular-devkit/architect'
-import { from, Observable, tap } from 'rxjs'
+import { Observable } from 'rxjs'
 
 /**
  * For compatibility reasons with the new nx versions.
  */
 export function toObservable<T extends BuilderOutput> (promiseOrAsyncIterator: Promise<T> | AsyncIterableIterator<T>): Observable<T | any> {
   if (typeof (promiseOrAsyncIterator as any).then === 'function') {
-    return from(promiseOrAsyncIterator as Promise<T>).pipe(
-      tap((result) => {
-        if (!result.success && result.error) {
-          throw result.error
-        }
-      })
-    )
+    return new Observable((subscriber) => {
+      (promiseOrAsyncIterator as Promise<T>)
+        .then((result) => {
+          if (!result.success && result.error) {
+            subscriber.error(result.error)
+          }
+
+          subscriber.next(result.value)
+
+          subscriber.complete()
+        })
+        .catch((e: any) => {
+          subscriber.error(e)
+        })
+    })
   } else {
     return new Observable((subscriber) => {
       const asyncIterator = promiseOrAsyncIterator as AsyncIterableIterator<T>
