595e228c2b9dce90c0e36c0063d2d180642595e5
Cenk Kılıç <cenk@kilic.dev>
2022-01-05 15:43:56 +0100

    fix: change injection method for nein bug

--- packages/nestjs-keycloak/src/admin/admin.module.ts
+++ packages/nestjs-keycloak/src/admin/admin.module.ts
@@ -1,4 +1,4 @@
-import { DynamicModule, Global, Module, Provider } from '@nestjs/common'
+import { DynamicModule, Global, Module } from '@nestjs/common'
 
 import { KEYCLOAK_ADMIN_INSTANCE, KEYCLOAK_ADMIN_OPTIONS } from './admin.constants'
 import { KeycloakAdminOptions } from './admin.interfaces'
@@ -11,22 +11,21 @@ import { KeycloakAdminService } from './admin.service'
 @Global()
 @Module({})
 export class KeycloakAdminModule {
-  protected static keycloakProvider: Provider = {
-    provide: KEYCLOAK_ADMIN_INSTANCE,
-    useFactory: (options: KeycloakAdminOptions) => new KeycloakAdminService(options),
-    inject: [ KEYCLOAK_ADMIN_OPTIONS ]
-  }
-
   static register (options: KeycloakAdminOptions): DynamicModule {
-    const providerOptions = {
-      provide: KEYCLOAK_ADMIN_OPTIONS,
-      useValue: options
-    }
-
     return {
       module: KeycloakAdminModule,
-      providers: [ providerOptions, this.keycloakProvider ],
-      exports: [ providerOptions, this.keycloakProvider ]
+      providers: [
+        {
+          provide: KEYCLOAK_ADMIN_OPTIONS,
+          useValue: options
+        },
+        {
+          provide: KEYCLOAK_ADMIN_INSTANCE,
+          useFactory: (options: KeycloakAdminOptions): KeycloakAdminService => new KeycloakAdminService(options),
+          inject: [ KEYCLOAK_ADMIN_OPTIONS ]
+        }
+      ],
+      exports: [ KEYCLOAK_ADMIN_OPTIONS, KEYCLOAK_ADMIN_INSTANCE ]
     }
   }
 }
