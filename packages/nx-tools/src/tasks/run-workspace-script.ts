ebcb8414454147c8071a86a8c8b480c048fbf48b
Cenk Kılıç <cenk@kilic.dev>
2022-01-26 18:02:10 +0100

    fix: directory manipulation for install

--- packages/nx-tools/src/tasks/run-workspace-script.ts
+++ packages/nx-tools/src/tasks/run-workspace-script.ts
@@ -11,11 +11,17 @@ import { Logger } from '@utils/logger'
  */
 export function addRunWorkspaceScriptTask (context: SchematicContext, options: PackageCommandSchema, dependencies?: TaskId[]): TaskId {
   const logger = new Logger(context)
-  process.chdir(options.root)
+  const cwd = process.cwd()
 
+  process.chdir(options.root)
   logger.debug(`Process current working directory has been changed: ${options.root}`)
 
-  return addExternalSchematicTask<PackageCommandSchema>(context, '@webundsoehne/nx-tools', 'package-script', options, dependencies)
+  const id = addExternalSchematicTask<PackageCommandSchema>(context, '@webundsoehne/nx-tools', 'package-script', options, dependencies)
+
+  logger.debug(`Process current working directory has been reverted: ${cwd}`)
+  process.chdir(cwd)
+
+  return id
 }
 
 export function addRunWorkspaceScriptTaskRule (options: PackageCommandSchema, dependencies?: TaskId[]): Rule {
