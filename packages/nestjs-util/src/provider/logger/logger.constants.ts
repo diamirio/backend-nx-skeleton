ad117d69280c8fe78ef50ec0a85937baebc8b92c
DablS <stephan@tailored-apps.com>
2022-02-17 15:55:35 +0100

    feat: allow multiple winston-transports next to console

--- packages/nestjs-util/src/provider/logger/logger.constants.ts
+++ packages/nestjs-util/src/provider/logger/logger.constants.ts
@@ -1,9 +1,21 @@
+import colors from 'cli-color'
+
 export enum LogType {
   error = 'error',
   log = 'info',
+  info = 'info',
   warn = 'warn',
   debug = 'debug',
-  verbose = 'verbose'
+  verbose = 'verbose',
+  none = 'none'
 }
 
 export const LOG_LEVEL: string = LogType.verbose
+
+export const ColorSchema = {
+  [LogType.log]: colors.greenBright,
+  [LogType.error]: colors.red,
+  [LogType.warn]: colors.yellow,
+  [LogType.debug]: colors.magentaBright,
+  [LogType.verbose]: colors.cyanBright
+}
--- packages/nestjs-util/src/provider/logger/logger.service.ts
+++ packages/nestjs-util/src/provider/logger/logger.service.ts
@@ -1,15 +1,41 @@
 import type { LoggerService as LoggerServiceCommon } from '@nestjs/common'
 import winston from 'winston'
+import Transport from 'winston-transport'
 
-import { LogType, LOG_LEVEL } from './logger.constants'
-import { Configurable, ConfigParam } from '@provider/config'
-
-const { format } = winston
+import { ColorSchema, LogType, LOG_LEVEL } from './logger.constants'
+import { ConfigParam, Configurable } from '@provider/config'
 
 let logger: winston.Logger
 
 export class LoggerService implements LoggerServiceCommon {
+  private transports: Transport[] = [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        winston.format.splat(),
+        winston.format.printf(
+          (data) =>
+            `[${data.timestamp}]` + // timestamp
+            ` ${ColorSchema[data.level](`[${data.level}]`)}` + // log-level
+            (data.context ? ` [${data.context}]` : '') + // context (optional)
+            ` - ${ColorSchema[data.level](data.message || 'missing log message')}` + // message
+            (data.ms ? ` ${data.ms}` : '') // ms since last log (optional)
+        )
+      )
+    })
+  ]
+
   constructor (private readonly context?: string) {}
+
+  addTransport (transport: Transport): this {
+    if (transport instanceof Transport) {
+      this.transports.push(transport)
+    }
+
+    return this
+  }
 
   error (message: any, trace?: string, context?: string): void {
     this.logMessage({
@@ -53,23 +79,18 @@ export class LoggerService implements LoggerServiceCommon {
   }
 
   @Configurable()
-  private getLogger (@ConfigParam('logLevel', LOG_LEVEL) level?: string): winston.Logger {
+  private getLogger (@ConfigParam('logLevel', LOG_LEVEL) logLevel?: string): winston.Logger {
     if (!logger) {
-      const loggingDisabled: boolean = level.toLowerCase() === 'none'
+      const level = LogType[logLevel.toLowerCase()] || LOG_LEVEL
 
       logger = winston.createLogger({
         level,
-        transports: [
-          new winston.transports.Console({
-            silent: loggingDisabled,
-            stderrLevels: ['error']
-          })
-        ],
-        format: format.combine(
-          format.timestamp(),
-          format.splat(),
-          format.printf((data) => `[${data.timestamp}] [${data.level}] [${data.context}] - ${data.message}`)
-        )
+        transports: this.transports.map((transport) => {
+          transport.level = transport.level || level
+          transport.silent = transport.level === LogType.none
+
+          return transport
+        })
       })
     }
 
