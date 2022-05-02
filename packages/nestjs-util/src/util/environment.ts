f5786eecc9afe8be78b6778dd8fb4f163b809cfe
Mattia Vecchio <mv@webundsoehne.com>
2022-05-02 18:02:38 +0200

    fix: provide further fallbacks to be really sure env vars are set

--- packages/nestjs-util/src/util/environment.ts
+++ packages/nestjs-util/src/util/environment.ts
@@ -2,12 +2,21 @@ import fs from 'fs-extra'
 import path from 'path'
 
 export async function setEnvironmentVariables (packageFile = path.join(process.cwd(), 'package.json')): Promise<void> {
-  const { name, version } = await fs.readJSON(packageFile)
+  // this should cover many cases and different package managers with various fallbacks
+  const name = process.env.PACKAGE_NAME ?? process.env.npm_package_name ?? path.basename(process.env.PWD ?? '').length ? path.basename(process.env.PWD) : undefined
+  const version = process.env.PACKAGE_VERSION ?? process.env.npm_package_version
 
-  process.env.PACKAGE_NAME = name
-  process.env.PACKAGE_VERSION = version
+  let packageJson
 
-  process.title = `${name}-${version}`
+  // only read package file if we miss something
+  if (!name || !version) {
+    packageJson = await fs.readJSON(packageFile)
+  }
+
+  process.env.PACKAGE_NAME = name ?? packageJson?.name ?? 'nestjs'
+  process.env.PACKAGE_VERSION = version ?? packageJson?.version ?? '0.0.0'
+
+  process.title = `${process.env.PACKAGE_NAME}-${process.env.PACKAGE_VERSION}`
 }
 
 export function requireNodeEnv (errorMessage = 'NODE_ENV environment variable is not set.'): void {
