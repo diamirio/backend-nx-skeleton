70a38dbd11f084dd257b98d5ed5d2530375a13d3
Cenk Kılıç <cenk@kilic.dev>
2022-05-30 11:24:06 +0200

    fix: change the seed signature to inject stuff in to it

--- packages/nestjs-mongodb-seeder/src/interfaces/mongodb-seed.interface.ts
+++ packages/nestjs-mongodb-seeder/src/interfaces/mongodb-seed.interface.ts
@@ -1,5 +1,3 @@
-import type { Connection } from 'mongoose'
-
 import type { MongoDBSeed } from './mongodb-seed'
 
-export type MongoDBSeeds = Record<PropertyKey, new (connection: Connection) => MongoDBSeed>
+export type MongoDBSeeds = Record<PropertyKey, new (...args: any[]) => MongoDBSeed>
