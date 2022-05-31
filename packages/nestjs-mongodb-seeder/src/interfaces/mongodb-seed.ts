f2b6fb78e80083f2020a662c319af330d3d7e665
Cenk Kılıç <cenk@kilic.dev>
2022-05-31 13:27:35 +0200

    fix: delete requirement for constructor inject

--- packages/nestjs-mongodb-seeder/src/interfaces/mongodb-seed.ts
+++ packages/nestjs-mongodb-seeder/src/interfaces/mongodb-seed.ts
@@ -1,13 +1,10 @@
 import { Logger } from '@nestjs/common'
-import type { Connection } from 'mongoose'
 
 /**
  * A singular seed that should be extended from, for seeding MongoDB.
  */
 export abstract class MongoDBSeed {
   protected logger: Logger = new Logger(this.constructor.name)
-
-  constructor (protected client: Connection) {}
 
   abstract run (): Promise<void>
 }
