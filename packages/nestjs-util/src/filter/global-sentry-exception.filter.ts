b4bd048daeeccff17b662225b4d40bf7b8b8e5f4
Mattia Vecchio <mv@webundsoehne.com>
2022-06-07 20:26:06 +0200

    fix: dynamically import sentry to avoid package missing error on optional usage

--- packages/nestjs-util/src/filter/global-sentry-exception.filter.ts
+++ packages/nestjs-util/src/filter/global-sentry-exception.filter.ts
@@ -1,16 +1,16 @@
 import type { ArgumentsHost, ExceptionFilter, OnApplicationShutdown } from '@nestjs/common'
 import { Catch } from '@nestjs/common'
-import * as Sentry from '@sentry/node'
-import type { Integration, Options } from '@sentry/types'
 import type { FastifyReply } from 'fastify'
 
+// please do not use static imports from @sentry here, as it's an optional dependency of the module
 import { ConfigService } from '../provider'
 import { GlobalExceptionFilter } from './global-exception.filter'
 
 @Catch()
 export class GlobalSentryExceptionFilter extends GlobalExceptionFilter implements ExceptionFilter, OnApplicationShutdown {
+  private sentry
   private initialized = false
-  private readonly options?: Options = ConfigService.get('sentry')
+  private readonly options? = ConfigService.get('sentry')
 
   constructor () {
     super()
@@ -26,37 +26,47 @@ export class GlobalSentryExceptionFilter extends GlobalExceptionFilter implement
       this.options.environment = `${process.env?.PACKAGE_NAME ? process.env?.PACKAGE_NAME + '_' : ''}${process.env?.NODE_ENV ?? 'any'}`
     }
 
-    const integrations: Integration[] = [
-      new Sentry.Integrations.OnUncaughtException({
-        onFatalError: async (error): Promise<void> => {
-          if (error.name === 'SentryError') {
-            this.logger.warn(`SentryError: ${JSON.stringify(error, null, 2)}`)
-          } else {
-            Sentry.getCurrentHub()?.getClient()?.captureException(error)
-          }
-        }
-      }),
-      new Sentry.Integrations.OnUnhandledRejection({ mode: 'warn' })
-    ]
+    // dynamically import sentry if available. typing has been omitted to avoid additional static imports
+    try {
+      this.sentry = require('@sentry/node')
+    } catch (e) {
+      this.logger.warn('Please install @sentry/node as dependency in your project.')
+      this.logger.warn('Reporting to sentry.io will remain disabled...')
 
-    Sentry.init({
-      ...this.options,
-      integrations
-    })
+      return
+    }
+
+    try {
+      const integrations: any[] = [
+        new this.sentry.Integrations.OnUncaughtException({
+          onFatalError: async (error): Promise<void> => {
+            if (error.name === 'SentryError') {
+              this.logger.warn(`SentryError: ${JSON.stringify(error, null, 2)}`)
+            } else {
+              this.sentry.getCurrentHub()?.getClient()?.captureException(error)
+            }
+          }
+        }),
+        new this.sentry.Integrations.OnUnhandledRejection({ mode: 'warn' })
+      ]
+
+      this.sentry.init({
+        ...this.options,
+        integrations
+      })
+    } catch (e) {
+      this.logger.warn('Sentry.io initialization failed: ' + e.toString())
+      this.logger.warn('Reporting to sentry.io will remain disabled...')
+
+      return
+    }
+
     this.initialized = true
-    this.logger.log('Reporting to sentry.io initialized successfully')
+    this.logger.log('Sentry.io initialized successfully. Uncaught exceptions will be reported...')
   }
 
   catch (exception: Error, host: ArgumentsHost): void {
     super.catch(exception, host)
-
-    const ctx = host.switchToHttp()
-    const response: FastifyReply = ctx.getResponse()
-
-    // only report unhandled exceptions identified as such by base handler
-    if (response?.statusCode < 500) {
-      return
-    }
 
     if (!this.initialized) {
       this.logger.warn('Reporting to sentry.io disabled. Please check if sentry was initialized correctly on startup.')
@@ -64,14 +74,22 @@ export class GlobalSentryExceptionFilter extends GlobalExceptionFilter implement
       return
     }
 
+    // only report unhandled exceptions identified as such by base handler
+    const ctx = host.switchToHttp()
+    const response: FastifyReply = ctx.getResponse()
+
+    if (response?.statusCode < 500) {
+      return
+    }
+
     try {
-      Sentry.captureException(exception)
+      this.sentry.captureException(exception)
     } catch (sentryException) {
       this.logger.warn(`Sentry error: ${JSON.stringify(sentryException.message, null, 2)}`)
     }
   }
 
   async onApplicationShutdown (): Promise<void> {
-    await Sentry.close()
+    await this.sentry.close()
   }
 }
