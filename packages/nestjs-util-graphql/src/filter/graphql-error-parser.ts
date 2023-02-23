660f30ccf635fed8791b9e74f726b695caabedbb
Cenk Kılıç <cenk@kilic.dev>
2023-02-23 15:48:01 +0100

    fix(nestjs-util): update graphql error parsing to handle direct graphql errors

--- packages/nestjs-util-graphql/src/filter/graphql-error-parser.ts
+++ packages/nestjs-util-graphql/src/filter/graphql-error-parser.ts
@@ -1,9 +1,20 @@
-import type { GraphQLError, GraphQLFormattedError } from 'graphql/error'
+import type { GraphQLFormattedError } from 'graphql/error'
+import { GraphQLError } from 'graphql/error'
 
 import { GraphQLPreformattedException } from './interface'
 import type { EnrichedException } from '@webundsoehne/nestjs-util'
 
 export function GraphQLErrorParser (exception: GraphQLError): GraphQLFormattedError {
+  // then this means that this does not come from the nestjs part of the application
+  // it is directly a graphql error
+  if (!exception?.extensions?.response) {
+    delete exception.extensions?.exception
+
+    exception.extensions.code = GraphQLError.name
+
+    return exception
+  }
+
   const extensions = exception.extensions.response as EnrichedException
 
   const message = structuredClone(extensions.message)
