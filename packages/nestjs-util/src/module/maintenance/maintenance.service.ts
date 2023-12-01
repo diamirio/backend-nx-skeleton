72c354604b67a948d2f70e20a34af084ce8b23fe
Cenk Kılıç <cenk@kilic.dev>
2023-12-01 20:01:27 +0100

    fix: maintancence mode

--- packages/nestjs-util/src/module/maintenance/maintenance.service.ts
+++ packages/nestjs-util/src/module/maintenance/maintenance.service.ts
@@ -31,12 +31,14 @@ export class MaintenanceService {
     if (!await this.isEnabled()) {
       this.logger.verbose(['Enabling maintenance mode (lockfile is %s)', this.lockfile])
 
-      await fs.writeFile(this.lockfile, '')
+      await fs.writeFile(this.lockfile, '', { encoding: 'utf8' })
 
       this.logger.log('◆◆◆ Maintenance mode enabled ◆◆◆')
-    } else {
-      this.logger.verbose('Maintenance mode already enabled.')
+
+      return
     }
+
+    this.logger.verbose('Maintenance mode already enabled.')
   }
 
   async disable (task = 'unknown-task'): Promise<void> {
@@ -51,9 +53,11 @@ export class MaintenanceService {
       })
 
       this.logger.log('◆◆◆ Maintenance mode disabled ◆◆◆')
-    } else {
-      this.logger.verbose(['Not disabling maintenance mode still got running tasks: %s', this.tasks.join(', ')])
+
+      return
     }
+
+    this.logger.verbose(['Not disabling maintenance mode still got running tasks: %s', this.tasks.join(', ')])
   }
 
   async isEnabled (): Promise<boolean> {
