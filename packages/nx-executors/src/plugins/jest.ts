058728ef849073da07cbee76881138b4922dace2
Philipp Hiegetsberger <philipp.hiegetsberger@diamir.io>
2024-07-05 07:09:15 +0200

    fix(nx-executors): disable `detectOpenHandles` for jest

--- packages/nx-executors/src/plugins/jest.ts
+++ packages/nx-executors/src/plugins/jest.ts
@@ -36,10 +36,10 @@ class JestPlugin extends PluginBuilder<JestPluginOptions> {
       options: {
         jestConfig: options.testConfig,
         passWithNoTests: true,
-        detectOpenHandles: true,
+        detectOpenHandles: false, // somehow results in "Cannot find module '@[...]' from [...]" if true
         noStackTrace: true,
         env: {
-          NODE_ENV: 'development', // config-package: hide NODE_ENV value of '..' did not match any deployment files
+          NODE_ENV: 'development', // config-package: hide NODE_ENV value of '[...'] did not match any deployment files
           SUPPRESS_NO_CONFIG_WARNING: 'true' // config-package: hide warning that config folder is missing
         }
       },
