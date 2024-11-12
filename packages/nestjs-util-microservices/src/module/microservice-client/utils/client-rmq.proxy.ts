aa63634dd1bbab1e1223130b42735afda5c1d39e
Philipp Hiegetsberger <philipp.hiegetsberger@diamir.io>
2024-11-12 15:35:17 +0100

    chore(nestjs-util-microservice): comment

--- packages/nestjs-util-microservices/src/module/microservice-client/utils/client-rmq.proxy.ts
+++ packages/nestjs-util-microservices/src/module/microservice-client/utils/client-rmq.proxy.ts
@@ -5,6 +5,12 @@ import type { AmqpConnectionManager } from 'amqp-connection-manager'
 export class ClientProxyRMQ extends ClientRMQ {
   handleError (client: AmqpConnectionManager): void {
     super.handleError(client)
+    /*
+     * closing the failed connection, to not block the client
+     * if the client sends a request to a not yet ready rabbitmq, the connection fails for any following request
+     * this will reopen a new connection on the next retry until the rabbitmq is ready
+     * see: https://github.com/nestjs/nest/issues/10562 for more information
+     */
     client.addListener(CONNECT_FAILED_EVENT, () => this.close())
   }
 }
